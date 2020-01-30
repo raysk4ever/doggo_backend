dcaf75adb7189435dbcb734231c148f234a98501
Ravi Kant Singh <raysk7161@gmail.com>
2020-01-31 03:47:41 +0530

    updated db connection string

--- startups/db.js
+++ startups/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () =>{
-    const url = 'mongodb://localhost:27017/test';
+    const url = 'mongodb+srv://ravi:' + process.env.dbPass +' @cluster0-7pwcx.mongodb.net/test?retryWrites=true&w=majority';
     mongoose.connect(url,  { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> console.log(`connected to mongodb`))
     .catch(err=> console.log(`Mongo Error: ${err}`));
